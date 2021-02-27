f128134ffd2d7cf08093c636a24f682f21c00614
soni <soni.shirwani@yahoo.com>
2021-02-27 12:34:01 +0530

    added

--- src/abstraction/AbstractionDemo.java
+++ src/abstraction/AbstractionDemo.java
@@ -5,6 +5,7 @@ public class AbstractionDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Demo could not be completed");
+      System.out.println("Try again");
 	}
 
 }
